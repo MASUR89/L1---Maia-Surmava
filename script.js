060d2031fc8399a36e3ad757e5f518cfc2009199
MASUR89 <surmava9@gmail.com>
2025-09-15 13:58:56 +0400

    HW 2

--- script.js
+++ script.js
@@ -1,59 +1,77 @@
-// let myString = 'Apple, Banana, Grape, Mango, Orange. '
+//1
+let basketString = 'Apple, Banana, Grape, Mango, Orange';
+let array = basketString.split(",").map(item => item.trim());
+console.log('Array:', array );
 
-// console.log(myString);
-// console.log('The string contains', myString.length, 'symbols');
-// console.log('Let us increase the letters:', myString.toLocaleUpperCase());
-// console.log('Let us decrease the letters:', myString.toLocaleLowerCase());
+//2
+console.log('Length:', basketString.length);
+console.log('High:', basketString.toUpperCase());
+console.log('Low:', basketString.toLowerCase());
 
-// let array = myString.split(",");
-// console.log('Array:', array );
+let string1 = basketString.replace('Banana' , 'Strawberry');
+console.log('New string:' , string1);
 
-// let newString1 = myString.replace('Banana' , 'Strawberry');
-// console.log('New string:' , newString1);
-
-// newString1.includes('Grape')
-//     ? console.log('Grape is in the list')
-//     : console.log('Grape is not in the list')
-
-// let doubleString = myString.repeat(2);
-// console.log(doubleString);
-
-//----------10.09.25-----------//
-// for (let i = 5; i < 20; i++) {
-//     console.log(i);
-// }
-
-// for(let i = 100; i > 5; i--) {
-//     console.log(i);
-// }
+if(basketString.startsWith('Apple')) console.log('List starts with "Apple"');
+if(basketString.endsWith('Orange')) console.log('List ends with "Orange"');
 
 
-// let count = 0;
-// do {
-//     console.log(count);
-//     count++;
-// } while (count < 10);
+let string2 = '';
+for(let i=0; i<array.length; i++) {
+    string2 += array[i];
+    if(i<array.length - 1) {
+        string2 += ' | '
+    }
+}
+console.log(string2);
 
-// let mySplit = 're:educate'
-// console.log(mySplit.split(':'));
+//3
+array.push('Pineapple');
+console.log('Added Pineapple',array);
 
-// let myNumber = 1545;
-// let result = myNumber >= 0 ? "Positive" : 'Negative';
-// console.log(result);
+array.pop();
+console.log('Removed last', array);
+
+array.unshift('Watermelon');
+console.log('Added first',array);
+
+array.shift();
+console.log('Removed first' ,array);
+
+//4
+basketString.includes('Grape')
+? console.log('Grape is available')
+: console.log('Grape is not available');
+
+let index = array.indexOf('Mango');
+console.log('Index of Mango:', index);
+
+let space = array.length > 5 ? 'The basket is full' : 'The basket has space';
+console.log(space);
+
+//5
+let fruit = prompt('Please enter a fruit name:');
+let index2 = array.indexOf(fruit);
+if(index2 !== -1) {
+    array.splice(index2, 1);
+    console.log(fruit + ' was removed from the basket.');
+} else {
+    array.push(fruit);
+    console.log(fruit + ' was added to the basket.');
+}
+console.log('Updated basket:', array);
+console.log('Reverse:', array.reverse())
 
 
-// let sum = 0;
 
-// for (let i = 1; i <= 100; i++) {
-//   sum += i;
-// }
-// console.log("Sum =", sum);
+let result = "";
 
-let toDoList = [ ];
-toDoList.push('Learn JavaScript');
-toDoList.push('Learn React');
-toDoList.push('Build a project');
-console.log(toDoList);
-toDoList.unshift('Urgent task');
-toDoList.pop();
-console.log(toDoList);
+for (let repeat = 0; repeat < 2; repeat++) {
+    for (let i = 0; i < array.length; i++) {
+        result += array[i];
+        if (!(repeat === 1 && i === array.length - 1)) {
+            result += " | ";
+        }
+    }
+}
+
+console.log(result);
